50f3c896aad3ce7a1d82f0bf22c20355c1481171
Pablo Rocha <pablorochattm@gmail.com>
2021-10-06 18:01:25 -0500

    added new contract

---
+++ contracts/Lottery.sol
@@ -0,0 +1,34 @@
+pragma solidity ^0.4.17;
+
+contract Lottery {
+    
+    address public manager;
+    address[] public players;
+    
+    modifier restricted(){
+        require(msg.sender == manager);
+        _;
+    }
+    
+    function Lottery() public {
+        manager = msg.sender;
+    }
+    
+    function getPlayers() public view returns(address[]){
+        return players;
+    }
+    
+    function enter() public payable{
+        require(msg.value > .01 ether);
+        players.push(msg.sender);
+    }
+    function random() private view returns(uint){
+        return uint(sha3(block.difficulty,players,now));
+    }
+    
+    function pickWinner() public restricted{
+        address winner = players[random()%players.length];
+        winner.transfer(this.balance);
+        players = new address[](0);
+    }
+}
